4bd7aa899b63c956db30519ed9f6598c49029b16
AtifAnsari0345 <atif25762@gmail.com>
2026-03-19 16:16:58 +0530

    Fix wildcard route for Node 22 compatibility

--- server/server.js
+++ server/server.js
@@ -34,7 +34,7 @@ app.use('/api/reviews', require('./routes/reviews'));
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../dist')));
 
-  app.get('*', (req, res) => {
+  app.get('/{*splat}', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../dist', 'index.html'));
   });
 }
