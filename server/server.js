3bc09514528b02d8bdeca13bd88e76b2625b4a4e
AtifAnsari0345 <atif25762@gmail.com>
2026-03-19 16:28:25 +0530

    Fix wildcard route for Render deploy

--- server/server.js
+++ server/server.js
@@ -34,7 +34,7 @@ app.use('/api/reviews', require('./routes/reviews'));
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../dist')));
 
-  app.get('/{*splat}', (req, res) => {
+  app.get('/:path(*)', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../dist', 'index.html'));
   });
 }
